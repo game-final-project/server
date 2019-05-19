821d1d3a189e117bb1381198c2f1faf0f2468dd6
willyprayogo26 <willyprayogo26@gmail.com>
2019-05-19 12:41:16 +0700

    change authenticate

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 const router = require('express').Router()
 const users = require('./users')
-const { isLogin } = require('../middlewares')
 const { UserController } = require('../controllers')
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 
-router.use(isLogin)
 router.use('/users', users)
 
 module.exports = router
